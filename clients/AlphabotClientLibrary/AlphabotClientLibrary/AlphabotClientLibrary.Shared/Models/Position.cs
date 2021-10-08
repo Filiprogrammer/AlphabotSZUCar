6e2bd1f392dc268c6d7278c13b90012f484d233f
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-08 13:39:59 +0200

    Create solution structure and add empty classes

---
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Models/Position.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlphabotClientLibrary.Shared.Models
+{
+    class Position
+    {
+    }
+}
