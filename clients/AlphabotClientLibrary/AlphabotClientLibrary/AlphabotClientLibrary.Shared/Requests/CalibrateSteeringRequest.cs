c8f51b4b3517a2aa635fd27a06edd6ec35f69d46
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-11 10:51:14 +0200

    Implement CalibrateSteeringRequest

---
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Requests/CalibrateSteeringRequest.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlphabotClientLibrary.Shared.Contracts;
+using AlphabotClientLibrary.Shared.Models;
+
+namespace AlphabotClientLibrary.Shared.Requests
+{
+    public class CalibrateSteeringRequest : IAlphabotRequest
+    {
+        public CalibrateSteeringRequest() { }
+
+        public BleInformation GetBleInformation()
+        {
+            Guid uuid = new Guid("d39e8d54-8019-46c8-a977-db13871bac59");
+            byte[] bytes = new byte[1];
+
+            bytes[0] = 0x01; //start steering calibration
+
+            return new BleInformation(uuid, bytes);
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] ret = new byte[1];
+           
+            ret[0] = 0x03; //Packet ID 0x03
+
+            return ret;
+        }
+    }
+}
