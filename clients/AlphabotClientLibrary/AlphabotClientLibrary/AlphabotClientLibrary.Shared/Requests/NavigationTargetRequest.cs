4c6178f2300c7554a6ab9e89a076b90ce881ca50
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-14 12:46:18 +0200

    Add NavigationTargetRequest

---
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Requests/NavigationTargetRequest.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlphabotClientLibrary.Shared.Contracts;
+using AlphabotClientLibrary.Shared.Models;
+
+namespace AlphabotClientLibrary.Shared.Requests
+{
+    public class NavigationTargetRequest : IAlphabotRequest
+    {
+        Position _position;
+
+        public NavigationTargetRequest(Position position)
+        {
+            _position = position;
+        }
+
+        public BleInformation GetBleInformation()
+        {
+            Guid uuid = new Guid("f56f0a15-52ae-4ad5-bfe1-557eed983618");
+            byte[] bytes = _position.ToByteArray();
+
+            return new BleInformation(uuid, bytes);
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] packetId = { 0x08 };
+
+            byte[] positionData = _position.ToByteArray();
+
+            return packetId.Concat(positionData).ToArray();
+        }
+    }
+}
