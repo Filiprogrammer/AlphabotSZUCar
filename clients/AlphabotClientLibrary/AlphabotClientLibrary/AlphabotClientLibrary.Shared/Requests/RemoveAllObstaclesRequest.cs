decbcd2733606cfb2c860280faf623470fe79bb8
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-14 12:46:29 +0200

    Add RemoveAllObstaclesRequest

---
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Requests/RemoveAllObstaclesRequest.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlphabotClientLibrary.Shared.Contracts;
+using AlphabotClientLibrary.Shared.Models;
+
+namespace AlphabotClientLibrary.Shared.Requests
+{
+    public class RemoveAllObstaclesRequest : IAlphabotRequest
+    {
+        public RemoveAllObstaclesRequest() { }
+
+        public BleInformation GetBleInformation()
+        {
+            //Not added in documentaion?
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] packetId = { 0x0C };
+
+            return packetId;
+        }
+    }
+}
