c3c2a80e3c8d7b3393cb754146a56b1058476976
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-14 12:42:39 +0200

    Add ToByteArray method in Position class

--- clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Models/Position.cs
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Models/Position.cs
@@ -8,13 +8,38 @@ namespace AlphabotClientLibrary.Shared.Models
 {
     public class Position
     {
+        /// <summary>
+        /// X coordinate in centimetres
+        /// </summary>
         public short PositionX { get; private set; }
+        /// <summary>
+        /// Y coordinate in centimetres
+        /// </summary>
         public short PositionY { get; private set; }
 
         public Position(short x, short y)
         {
             PositionX = x;
             PositionY = y;
+        }
+
+        /// <summary>
+        /// Converts the position in a byte array, 
+        /// </summary>
+        /// <returns>byte[0-1]: PositionX (short), byte[2-3]: PositionY (short)</returns>
+        public byte[] ToByteArray()
+        {
+            byte[] ret = new byte[4];
+
+            byte[] distanceXInBytes = BitConverter.GetBytes(PositionX);
+            byte[] distanceYInBytes = BitConverter.GetBytes(PositionY);
+
+            ret[0] = distanceXInBytes[1];
+            ret[1] = distanceXInBytes[0];
+            ret[2] = distanceYInBytes[1];
+            ret[3] = distanceYInBytes[0];
+
+            return ret;
         }
     }
 }
