380bb56e1eb99942a547123c029f4cf28a96a671
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-14 12:46:08 +0200

    Add ConfigurePositioningAnchorRequest

---
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Requests/ConfigurePositioningAnchorRequest.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlphabotClientLibrary.Shared.Contracts;
+using AlphabotClientLibrary.Shared.Models;
+
+namespace AlphabotClientLibrary.Shared.Requests
+{
+    public class ConfigurePositioningAnchorRequest : IAlphabotRequest
+    {
+        byte _anchorId;
+        Position _position;
+
+        public ConfigurePositioningAnchorRequest(byte anchorId, Position position)
+        {
+            _anchorId = anchorId;
+            _position = position;
+        }
+
+        public BleInformation GetBleInformation()
+        {
+            //aktuell keine Ble Implementation/Dokumentation?
+
+            throw new NotImplementedException();
+        }
+
+        public byte[] GetBytes()
+        {
+            byte[] packetId = { 0x07 }; //packet id 0x07
+
+            byte[] data = GetDataBytes();
+
+            return packetId.Concat(data).ToArray();
+        }
+
+        private byte[] GetDataBytes()
+        {
+            byte[] ret = new byte[5];
+
+            byte[] anchorId = { _anchorId };
+
+            byte[] positionData = _position.ToByteArray();
+
+            ret = anchorId.Concat(positionData).ToArray();
+
+            return ret;
+        }
+    }
+}
