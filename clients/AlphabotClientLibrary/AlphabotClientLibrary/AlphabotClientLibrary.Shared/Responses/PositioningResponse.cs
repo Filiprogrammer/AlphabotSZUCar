3173c79f6543ff64c7d96f235479db4317325c3c
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-15 21:30:46 +0200

    Add PositioningResponse

---
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Responses/PositioningResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlphabotClientLibrary.Shared.Contracts;
+using AlphabotClientLibrary.Shared.Models;
+
+namespace AlphabotClientLibrary.Shared.Responses
+{
+    public class PositioningResponse : IAlphabotResponse
+    {
+        public Position Position { get; private set; }
+
+        public PositioningResponse(Position position)
+        {
+            Position = position;
+        }
+    }
+}
