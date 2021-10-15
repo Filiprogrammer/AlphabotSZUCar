37137ef83f74d361f95e4d12523c603172c152e2
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-15 21:32:55 +0200

    Add MultipleSensorResponse

---
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Responses/MultipleSensorResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlphabotClientLibrary.Shared.Contracts;
+
+namespace AlphabotClientLibrary.Shared.Responses
+{
+    public class MultipleSensorResponse : IAlphabotResponse
+    {
+        public List<IAlphabotResponse> SensorResponses { get; private set; }
+
+        public MultipleSensorResponse(List<IAlphabotResponse> sensorResponses)
+        {
+            SensorResponses = sensorResponses;
+        }
+    }
+}
