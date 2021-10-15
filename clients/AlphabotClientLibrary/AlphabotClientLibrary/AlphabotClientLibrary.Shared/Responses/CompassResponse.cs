d074c61d70ede58aa60a5286f3ece3ea529fbdbf
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-15 21:31:58 +0200

    Add CompassResponse

---
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Responses/CompassResponse.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlphabotClientLibrary.Shared.Contracts;
+
+namespace AlphabotClientLibrary.Shared.Responses
+{
+    public class CompassResponse : IAlphabotResponse
+    {
+        public short Degree { get; private set; }
+
+        public CompassResponse(short degree)
+        {
+            Degree = degree;
+        }
+    }
+}
