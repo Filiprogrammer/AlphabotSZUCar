b2a06b032d5d9faf7851957f5d31d0fc304d0d28
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-15 21:31:22 +0200

    Add NewObstacleRegisteredResponse

---
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Responses/NewObstacleRegisteredResponse.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlphabotClientLibrary.Shared.Contracts;
+using AlphabotClientLibrary.Shared.Models;
+
+namespace AlphabotClientLibrary.Shared.Responses
+{
+    public class NewObstacleRegisteredResponse : IAlphabotResponse
+    {
+        public Obstacle Obstacle { get; private set; }
+        
+        public NewObstacleRegisteredResponse(Obstacle obstacle)
+        {
+            Obstacle = obstacle;
+        }
+    }
+}
