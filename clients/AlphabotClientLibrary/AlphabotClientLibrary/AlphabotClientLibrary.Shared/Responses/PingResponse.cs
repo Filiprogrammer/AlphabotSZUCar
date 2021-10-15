a68372c818caca085f45f858fb2ac6f593496573
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-15 21:31:00 +0200

    Add PingResponse

---
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Responses/PingResponse.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlphabotClientLibrary.Shared.Contracts;
+
+namespace AlphabotClientLibrary.Shared.Responses
+{
+    public class PingResponse : IAlphabotResponse
+    {
+        public long Time { get; private set; }
+        public PingResponse(long time)
+        {
+            Time = time;
+        }
+    }
+}
