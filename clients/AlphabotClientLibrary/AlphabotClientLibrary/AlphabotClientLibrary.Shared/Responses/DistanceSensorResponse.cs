ec291ce8f43afd4a20d8b0ac33161dd9f9b225cd
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-15 21:31:47 +0200

    Add DistanceSensorResponse

---
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Responses/DistanceSensorResponse.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlphabotClientLibrary.Shared.Contracts;
+
+namespace AlphabotClientLibrary.Shared.Responses
+{
+    public class DistanceSensorResponse : IAlphabotResponse
+    {
+        public short Degree { get; private set; }
+        public ushort Distance { get; private set; }
+
+        public DistanceSensorResponse(short degree, ushort distance)
+        {
+            Degree = degree;
+            Distance = distance;
+        }
+    }
+}
