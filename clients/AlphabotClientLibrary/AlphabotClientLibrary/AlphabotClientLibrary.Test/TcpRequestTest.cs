016df7126236314a447eb8b24964d64fac556f8a
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-10-18 18:24:24 +0200

    Add AddObstacleRequest test

---
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Test/TcpRequestTest.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using AlphabotClientLibrary.Shared.Models;
+using AlphabotClientLibrary.Shared.Requests;
+using Xunit;
+
+namespace AlphabotClientLibrary.Test
+{
+    public class TcpRequestTest
+    {
+        [Fact]
+        public void TestAddObstacleRequest()
+        {
+            Obstacle obstacle = new Obstacle(new Position(40, 10523), 415, 101);
+            AddObstacleRequest request = new AddObstacleRequest(obstacle);
+
+            byte[] expectedBytes = { 9, 40,0, 27, 41, 159, 1 , 101, 0};
+            Assert.Equal(request.GetBytes(), expectedBytes);
+        }
+    }
+}
