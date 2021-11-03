20b39a81c97c05f8bebc2f6c7dee41497a50646b
Filiprogrammer <44641787+Filiprogrammer@users.noreply.github.com>
2021-11-03 22:30:33 +0100

    esp32/PrimaryESP32: Fix inverted differential

--- esp32/PrimaryESP32/alphabot/TwoMotorDrive.cpp
+++ esp32/PrimaryESP32/alphabot/TwoMotorDrive.cpp
@@ -37,8 +37,8 @@ void TwoMotorDrive::updateMotors(int8_t x, int8_t y) {
 
     float dir = 35.0 * tan((double)x / 250.0);
 
-    speed_left -= ((double)y / 127.0) * MIN(dir, 0.0);
-    speed_right += ((double)y / 127.0) * MAX(dir, 0.0);
+    speed_left += ((double)y / 127.0) * MIN(dir, 0.0);
+    speed_right -= ((double)y / 127.0) * MAX(dir, 0.0);
 
     if (speed_left > 0)
         this->motor_left->forward();
