0cb8046814f77d4b33cc68afd7684668e5566686
Marcel Fürpaß <22198605+LecraM2003@users.noreply.github.com>
2021-11-10 10:19:51 +0100

    Update clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Responses/MultipleSensorResponse.cs
    Co-authored-by: Filiprogrammer <44641787+Filiprogrammer@users.noreply.github.com>

--- clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Responses/MultipleSensorResponse.cs
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Responses/MultipleSensorResponse.cs
@@ -6,6 +6,14 @@ namespace AlphabotClientLibrary.Shared.Responses
 {
     public class MultipleSensorResponse : IAlphabotResponse
     {
+        public enum SensorType
+        {
+            None,
+            DistanceSensor,
+            Positioning,
+            Compass
+        }
+
         public List<IAlphabotResponse> SensorResponses { get; private set; }
 
         public MultipleSensorResponse(List<IAlphabotResponse> sensorResponses)
