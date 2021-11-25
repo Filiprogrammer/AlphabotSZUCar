94c6499eaa495e3763d8412c147858e4315d55d9
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-11-25 17:54:51 +0100

    Add AlphabotResponseType enum

--- clients/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Contracts/IAlphabotResponse.cs
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary.Shared/Contracts/IAlphabotResponse.cs
@@ -2,6 +2,19 @@
 
 namespace AlphabotClientLibrary.Shared.Contracts
 {
+    public enum AlphabotResponseType
+    {
+        Compass,
+        DistanceSensor,
+        Error,
+        MultipleSensor,
+        NewObstacle,
+        PathFinding,
+        Ping,
+        Positioning,
+        Toggle
+    }
+
     public interface IAlphabotResponse
     {
     }
