a0ab8e58c825d496d75e00d9dbb541d3c74f1e8d
Marcel Fürpaß <22198605+LecraM2003@users.noreply.github.com>
2021-12-06 14:16:49 +0100

    Update clients/AlphabotClientLibrary/AlphabotClientLibrary.Example.WinForms/MainForm.cs
    Co-authored-by: Filiprogrammer <44641787+Filiprogrammer@users.noreply.github.com>

--- clients/AlphabotClientLibrary/AlphabotClientLibrary.Example.WinForms/MainForm.cs
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary.Example.WinForms/MainForm.cs
@@ -1,19 +1,12 @@
-﻿using AlphabotClientLibrary.Core;
+using System;
+using System.Net;
+using System.Threading;
+using System.Windows.Forms;
+using AlphabotClientLibrary.Core;
 using AlphabotClientLibrary.Core.Tcp;
 using AlphabotClientLibrary.Shared.Contracts;
 using AlphabotClientLibrary.Shared.Models;
 using AlphabotClientLibrary.Shared.Requests;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Net;
-using System.Text;
-using System.Threading;
-using System.Threading.Tasks;
-using System.Windows.Forms;
 
 namespace AlphabotClientLibrary.Example.WinForms
 {
