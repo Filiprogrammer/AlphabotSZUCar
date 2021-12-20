491fc8d3546a2509d68339014e782c66d137f996
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-12-20 15:38:09 +0100

    Adapt packet responses to TcpResponseInterpreter

--- clients/AlphabotClientLibrary/AlphabotClientLibrary.Core.Tcp/TcpResponseInterpreter.cs
+++ clients/AlphabotClientLibrary/AlphabotClientLibrary.Core.Tcp/TcpResponseInterpreter.cs
@@ -36,10 +36,45 @@ namespace AlphabotClientLibrary.Core.Tcp
                 case 0x07:
                     return GetToggleResponse();
                 case 0x08:
+                    return GetAnchorDistancesResponse();
+                case 0x09:
+                    return GetWheelSpeedResponse();
+                case 0x0A:
+                    return GetGyroscopeResponse();
+                case 0x0B:
+                    return GetAccelerometerResponse();
+                case 0x0C:
                     return GetErrorResponse();
             }
 
             throw new Exception("Received packet id is not valid");
+        }
+
+        private AccelerometerResponse GetAccelerometerResponse()
+        {
+            float[] axes = GetXYZAxes();
+            return new AccelerometerResponse(axes[0], axes[1], axes[2]);
+        }
+
+        private GyroscopeResponse GetGyroscopeResponse()
+        {
+            float[] axes = GetXYZAxes();
+            return new GyroscopeResponse(axes[0], axes[1], axes[2]);
+        }
+
+        private WheelSpeedResponse GetWheelSpeedResponse()
+        {
+            sbyte speed = (sbyte)(DataBytes[0]);
+            return new WheelSpeedResponse(speed);
+        }
+
+        private AnchorDistancesResponse GetAnchorDistancesResponse()
+        {
+            ushort distanceAnchor0 = (ushort)(DataBytes[0] | (DataBytes[1] << 8));
+            ushort distanceAnchor1 = (ushort)(DataBytes[2] | (DataBytes[3] << 8));
+            ushort distanceAnchor2 = (ushort)(DataBytes[4] | (DataBytes[5] << 8));
+
+            return new AnchorDistancesResponse(distanceAnchor0, distanceAnchor1, distanceAnchor2);
         }
 
         private DistanceSensorResponse GetDistanceSensorResponse()
@@ -62,6 +97,21 @@ namespace AlphabotClientLibrary.Core.Tcp
         {
             short degree = (short)(DataBytes[0] | (DataBytes[1] << 8));
             return new CompassResponse(degree);
+        }
+
+        private float[] GetXYZAxes()
+        {
+            byte[] xAxisBytes = new byte[4];
+            byte[] yAxisBytes = new byte[4];
+            byte[] zAxisBytes = new byte[4];
+            Array.Copy(DataBytes, 0, xAxisBytes, 0, 4);
+            Array.Copy(DataBytes, 4, yAxisBytes, 0, 4);
+            Array.Copy(DataBytes, 8, zAxisBytes, 0, 4);
+            float xAxis = BitConverter.ToSingle(xAxisBytes, 0);
+            float yAxis = BitConverter.ToSingle(yAxisBytes, 0);
+            float zAxis = BitConverter.ToSingle(zAxisBytes, 0);
+
+            return new float[] { xAxis, yAxis, zAxis };
         }
     }
 }
