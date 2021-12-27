437240a668aa626ab23813a5aaa39cf5b86f7191
LecraM2003 <22198605+LecraM2003@users.noreply.github.com>
2021-12-27 15:43:44 +0100

    Avoid duplicate code in SocketReader

--- rpi/Alphabot.Net/Alphabot.Net.Shared/SocketReader.cs
+++ rpi/Alphabot.Net/Alphabot.Net.Shared/SocketReader.cs
@@ -11,7 +11,7 @@ namespace Alphabot.Net.Shared
     /// <summary>
     /// This class receives incoming messages and encodes them to ASCII.
     /// </summary>
-    public class SocketReader: ISocketReader
+    public class SocketReader : ISocketReader
     {
         private readonly Socket _clientSocket;
         private readonly IServiceLogger _serviceLogger = ServiceLogger.GetInstance().Current;
@@ -35,26 +35,7 @@ namespace Alphabot.Net.Shared
         /// <returns>encoded message</returns>
         public string ReceiveText()
         {
-            byte[] msg = new byte[_prefs.ServiceSettings.SocketBufferSize];
-            try
-            {
-               var size= _clientSocket.Receive(msg);
-       
-               if (size == 0) // connection lost
-               {
-                   throw new TcpConnectionLostException("0 Bytes received: Tcp connection lost");
-               }
-               _serviceLogger.Log(LogLevel.Debug, "Shared.SocketReader.ReceiveText", $"bytes received: {size}" );
-            }
-            catch (SocketException sx)
-            {
-                _serviceLogger.Log(LogLevel.Information, "Shared.SocketWriter.ReceiveText", "SocketException: " + sx.Message);
-            }
-            catch (ObjectDisposedException ox)
-            {
-                _serviceLogger.Log(LogLevel.Information, "Shared.SocketWriter.ReceiveText", "ObjectDisposedException: " + ox.Message);
-
-            }
+            byte[] msg = ReceiveBytes();
             string s = Encoding.ASCII.GetString(msg);
             s = s.Substring(0, s.IndexOf('\0'));
             return s;
@@ -101,7 +82,7 @@ namespace Alphabot.Net.Shared
             _clientSocket.Shutdown(SocketShutdown.Both);
             _clientSocket.Close();
         }
-        
- 
+
+
     }
 }
