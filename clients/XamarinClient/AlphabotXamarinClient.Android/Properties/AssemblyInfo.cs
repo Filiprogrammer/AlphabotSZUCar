aa1e602de005c87bddfeabdf6760c08c928b8a01
Filiprogrammer <44641787+Filiprogrammer@users.noreply.github.com>
2022-01-02 21:10:16 +0100

    Rename App2 and App3 to AlphabotXamarinClient

--- clients/XamarinClient/AlphabotXamarinClient.Android/Properties/AssemblyInfo.cs
+++ clients/XamarinClient/AlphabotXamarinClient.Android/Properties/AssemblyInfo.cs
@@ -6,11 +6,11 @@ using Android.App;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("App3.Android")]
+[assembly: AssemblyTitle("AlphabotXamarinClient.Android")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("App3.Android")]
+[assembly: AssemblyProduct("AlphabotXamarinClient.Android")]
 [assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
--- clients/XamarinClient/AlphabotXamarinClient.UWP/Properties/AssemblyInfo.cs
+++ clients/XamarinClient/AlphabotXamarinClient.UWP/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("App2.UWP")]
+[assembly: AssemblyTitle("AlphabotXamarinClient.UWP")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("App2.UWP")]
+[assembly: AssemblyProduct("AlphabotXamarinClient.UWP")]
 [assembly: AssemblyCopyright("Copyright ©  2015")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
