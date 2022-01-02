c6838acd861dccf5c2fe210971ec3111c1916e44
Filiprogrammer <44641787+Filiprogrammer@users.noreply.github.com>
2022-01-02 22:50:19 +0100

    Fix broken reference in MainActivity for real

--- clients/XamarinClient/AlphabotXamarinClient.Android/MainActivity.cs
+++ clients/XamarinClient/AlphabotXamarinClient.Android/MainActivity.cs
@@ -15,10 +15,10 @@ namespace AlphabotXamarinClient.Android
 
             Xamarin.Essentials.Platform.Init(this, savedInstanceState);
             global::Xamarin.Forms.Forms.Init(this, savedInstanceState);
-            LoadApplication(new AlphabotXamarinClient.App());
+            LoadApplication(new App());
         }
 
-        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Android.Content.PM.Permission[] grantResults)
+        public override void OnRequestPermissionsResult(int requestCode, string[] permissions, [GeneratedEnum] Permission[] grantResults)
         {
             Xamarin.Essentials.Platform.OnRequestPermissionsResult(requestCode, permissions, grantResults);
 
