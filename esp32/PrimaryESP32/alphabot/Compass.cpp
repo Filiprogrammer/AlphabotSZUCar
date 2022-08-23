14fc4c91d2c87bfabc13977fc41807181f53b479
Filiprogrammer <44641787+Filiprogrammer@users.noreply.github.com>
2022-08-23 15:33:18 +0200

    Remove unused Mecha_QMC5883L library

--- esp32/PrimaryESP32/alphabot/Compass.cpp
+++ esp32/PrimaryESP32/alphabot/Compass.cpp
@@ -2,23 +2,8 @@
 #include "config.h"
 
 float Compass::getRawDirection() {
-    /*int16_t x, y, z;
-    magnet_sensor.read(&x, &y, &z);
-    x = map(x, min_x, max_x, -1000, 1000);
-    y = map(y, min_y, max_y, -1000, 1000);
-    float heading = atan2(y, x);
-    heading += COMPASS_DECLINATION;
-
-    if(heading < 0.0)
-        heading += 2 * PI;
-
-    else if(heading >= 2 * PI)
-        heading -= 2 * PI;*/
-
     motion_tracker->fetchOrientationData();
     return motion_tracker->getYaw();
-
-    //return (heading * 180.0 / PI);
 }
 
 void Compass::setMagnetSensorCalibratedValues(int16_t min_x, int16_t max_x, int16_t min_y, int16_t max_y) {
@@ -37,21 +22,6 @@ float Compass::getAngleOffset() const {
 }
 
 void Compass::magnetSensorCalibrate() {
-    /*int16_t x, y, z;
-    magnet_sensor.read(&x, &y, &z);
-
-    if (min_max_set) {
-        min_x = min(x, min_x);
-        max_x = max(x, max_x);
-        min_y = min(y, min_y);
-        max_y = max(y, max_y);
-    } else {
-        min_x = x;
-        max_x = x;
-        min_y = y;
-        max_y = y;
-        min_max_set = true;
-    }*/
 }
 
 void Compass::beginMagnetSensorCalibration() {
@@ -79,6 +49,5 @@ float Compass::getDirection() {
 }
 
 Compass::Compass() {
-    //magnet_sensor.init();
     motion_tracker = new MotionTracker();
 }
