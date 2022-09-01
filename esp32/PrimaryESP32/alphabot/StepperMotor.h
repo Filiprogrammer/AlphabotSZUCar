d8ab81fff67928131dcc0803952bba84f807c5f0
Filiprogrammer <44641787+Filiprogrammer@users.noreply.github.com>
2022-09-01 20:04:14 +0200

    esp32/PrimaryESP32: Move StepperMotor task to ULP (#94)
    Move the StepperMotor task to the Ultra Low Power (ULP) coprocessor of
    the ESP32. This reduces the load on the main processor while still
    ensuring that the steering task runs smoothly.

--- esp32/PrimaryESP32/alphabot/StepperMotor.h
+++ esp32/PrimaryESP32/alphabot/StepperMotor.h
@@ -5,29 +5,19 @@
 
 #define STEPPER_CALIBRATION_STEPS 625
 
-typedef enum {
-    inactive,
-    turning,
-    calibrating
-} StepperState;
-
 class StepperMotor {
 private:
     uint8_t pin_in1;
     uint8_t pin_in2;
     uint8_t pin_in3;
     uint8_t pin_in4;
-    int16_t current_direction;
     int16_t desired_direction;
-    StepperState state;
-    uint16_t calibration_step;
 
-    void stepperTask();
+    void startStepperTask();
 
 public:
     void turnTo(int16_t dir);
     void calibrate();
-    void startStepperTask();
 
     StepperMotor(uint8_t pin_in1, uint8_t pin_in2, uint8_t pin_in3, uint8_t pin_in4);
 };
