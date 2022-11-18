c9f665a339832fddf11c1940630586ad508a7050
Filiprogrammer <44641787+Filiprogrammer@users.noreply.github.com>
2022-11-18 10:37:34 +0100

    esp32/PrimaryESP32: Improve compass (#121)
    Improve compass by bringing back manual magnetometer calibration.

--- esp32/PrimaryESP32/alphabot/Compass.cpp
+++ esp32/PrimaryESP32/alphabot/Compass.cpp
@@ -1,9 +1,132 @@
 #include "Compass.h"
 #include "config.h"
 
+float fmap(float x, float in_min, float in_max, float out_min, float out_max) {
+    float dividend = out_max - out_min;
+    float divisor = in_max - in_min;
+    float delta = x - in_min;
+
+    if(divisor == 0)
+        return 0;
+
+    return (delta * dividend + (divisor / 2)) / divisor + out_min - 0.5;
+}
+
 float Compass::getRawDirection() {
     motion_tracker->fetchData();
-    return 360.0 - motion_tracker->getYaw();
+    float x = motion_tracker->getUncalMagX();
+    float y = motion_tracker->getUncalMagY();
+    float z = motion_tracker->getUncalMagZ();
+
+    x = fmap(x, min_x, max_x, -1.0, 1.0);
+    y = fmap(y, min_y, max_y, -1.0, 1.0);
+    z = fmap(z, min_z, max_z, -1.0, 1.0);
+
+    float heading = atan2(y, x);
+
+    if (heading < 0.0)
+        heading += 2 * PI;
+    else if (heading >= 2 * PI)
+        heading -= 2 * PI;
+
+    float mag_dir = heading * 180.0 / PI;
+    float quat6_dir = 360.0 - motion_tracker->getYaw6();
+
+    // If mag values go far beyond the min max values, do not let the mag contribute
+    if (fabs(x) > 1.5 || fabs(y) > 1.5 || fabs(z) > 1.5) {
+        #ifdef DEBUG
+        Serial.println("Magnetic interference detected");
+        #endif
+    } else {
+        double diff = fmod(mag_dir - quat6_dir - avg_diff_quat6_mag + 540, 360) - 180;
+        avg_diff_quat6_mag = fmod(360 + avg_diff_quat6_mag + diff / 1000, 360);
+    }
+
+    return fmod(avg_diff_quat6_mag + quat6_dir, 360);
+}
+
+void Compass::setMagnetSensorCalibratedValues(float min_x, float max_x, float min_y, float max_y, float min_z, float max_z) {
+    this->min_x = min_x;
+    this->max_x = max_x;
+    this->min_y = min_y;
+    this->max_y = max_y;
+    this->min_z = min_z;
+    this->max_z = max_z;
+}
+
+void Compass::magnetSensorCalibrate() {
+    motion_tracker->fetchData();
+    float x = motion_tracker->getUncalMagX();
+    float y = motion_tracker->getUncalMagY();
+    float z = motion_tracker->getUncalMagZ();
+
+    if (min_max_set) {
+        min_x = min(x, min_x);
+        max_x = max(x, max_x);
+        min_y = min(y, min_y);
+        max_y = max(y, max_y);
+        min_z = min(z, min_z);
+        max_z = max(z, max_z);
+    } else {
+        min_x = x;
+        max_x = x;
+        min_y = y;
+        max_y = y;
+        min_z = z;
+        max_z = z;
+        min_max_set = true;
+    }
+}
+
+void Compass::beginMagnetSensorCalibration() {
+    min_max_set = false;
+}
+
+void Compass::finishMagnetSensorCalibration() {
+    // Initialize avg_diff_quat6_mag quickly, to prevent having to wait for the value to converge.
+    motion_tracker->fetchData();
+    float x = motion_tracker->getUncalMagX();
+    float y = motion_tracker->getUncalMagY();
+
+    x = fmap(x, min_x, max_x, -1.0, 1.0);
+    y = fmap(y, min_y, max_y, -1.0, 1.0);
+
+    float heading = atan2(y, x);
+
+    if (heading < 0.0)
+        heading += 2 * PI;
+    else if (heading >= 2 * PI)
+        heading -= 2 * PI;
+
+    float mag_dir = heading * 180.0 / PI;
+    float quat6_dir = 360.0 - motion_tracker->getYaw6();
+
+    double diff = fmod(mag_dir - quat6_dir - avg_diff_quat6_mag + 540, 360) - 180;
+    avg_diff_quat6_mag = fmod(360 + avg_diff_quat6_mag + diff, 360);
+}
+
+float Compass::getMinX() const {
+    return min_x;
+}
+
+float Compass::getMaxX() const {
+    return max_x;
+}
+
+float Compass::getMinY() const {
+    return min_y;
+}
+
+float Compass::getMaxY() const {
+    return max_y;
+}
+
+float Compass::getMinZ() const {
+    return min_z;
+}
+
+float Compass::getMaxZ() const {
+    return max_z;
 }
 
 void Compass::setAngleOffset(float offset) {
--- esp32/PrimaryESP32/alphabot/alphabot.ino
+++ esp32/PrimaryESP32/alphabot/alphabot.ino
@@ -61,6 +61,7 @@ struct logging {
 } logging;
 
 bool connected = false;
+bool magnet_sensor_calibration = false;
 
 BLEHandler* ble_handler = NULL;
 BLECharacteristicSender* ble_char_toggle_sender = NULL;
@@ -159,8 +160,19 @@ void charCalibrateDataReceived(const char* data, size_t len) {
                     // TODO: Automatic magnetometer calibration
                     break;
                 case 2: // Begin manual magnetometer calibration
+                    compass->beginMagnetSensorCalibration();
+                    magnet_sensor_calibration = true;
                     break;
                 case 3: // Finish magnetometer calibration
+                    magnet_sensor_calibration = false;
+                    compass->finishMagnetSensorCalibration();
+                    save_file->setMagnetSensorCalibratedMinX(compass->getMinX());
+                    save_file->setMagnetSensorCalibratedMaxX(compass->getMaxX());
+                    save_file->setMagnetSensorCalibratedMinY(compass->getMinY());
+                    save_file->setMagnetSensorCalibratedMaxY(compass->getMaxY());
+                    save_file->setMagnetSensorCalibratedMinZ(compass->getMinZ());
+                    save_file->setMagnetSensorCalibratedMaxZ(compass->getMaxZ());
+                    save_file->write();
                     break;
                 case 4: // Calibrate compass direction
                     float correct_dir = atan2(positioning_system->getAnc1Y() - lps_y, positioning_system->getAnc1X() - lps_x) * (180.0 / PI);
@@ -293,10 +305,18 @@ void setup() {
     ble_char_toggle_sender = new BLECharacteristicSender(ble_handler->charToggle, &onCharToggleArrive);
     ble_char_add_obstacle_sender = new BLECharacteristicSender(ble_handler->charAddObstacle, &onCharAddObstacleArrive);
     positioning_system = new PositioningSystem(DW1000_ANCHOR_1_SHORT_ADDRESS, DW1000_ANCHOR_2_SHORT_ADDRESS, DW1000_ANCHOR_3_SHORT_ADDRESS);
-    motion_tracker = new MotionTracker(false, false, false, false, true);
+    motion_tracker = new MotionTracker(false, false, true, false, false, true, false);
     compass = new Compass(motion_tracker);
     save_file = new SaveFile();
+    compass->setMagnetSensorCalibratedValues(
+        save_file->getMagnetSensorCalibratedMinX(),
+        save_file->getMagnetSensorCalibratedMaxX(),
+        save_file->getMagnetSensorCalibratedMinY(),
+        save_file->getMagnetSensorCalibratedMaxY(),
+        save_file->getMagnetSensorCalibratedMinZ(),
+        save_file->getMagnetSensorCalibratedMaxZ());
     compass->setAngleOffset(save_file->getCompassAngleOffset());
+    compass->finishMagnetSensorCalibration();
     navigator = new Navigator(two_motor_drive);
 
     settings.explore_mode = false;
@@ -400,6 +420,9 @@ void loop() {
         Serial.println(back_dist);
         #endif
     }
+
+    if (magnet_sensor_calibration)
+        compass->magnetSensorCalibrate();
 
     if (logging.compass_direction || settings.positioning) {
         raw_dir = compass->getRawDirection();
